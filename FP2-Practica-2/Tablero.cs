a09706a30be1beea462abb0441271f3641931f2f
Andrea Aparicio <apariciolopez.andrea@gmail.com>
2024-04-03 22:19:10 +0200

    arreglao (creo)

---
+++ FP2-Practica-2/Tablero.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FP2P2
+{
+    class Tablero
+    {
+        // contenido de las casillas
+        enum Casilla { Libre, Muro, Comida, Vitamina, MuroCelda };
+        // matriz de casillas (tablero)
+        Casilla[,] cas;
+        // representacion de los personajes (pacman y fantasmas)
+        struct Personaje
+        {
+            public Coor pos, dir, // posicion y direccion actual
+            ini; // posicion inicial (para fantasmas)
+        }
+        // vector de personajes, 0 es pacman y el resto fantasmas
+        Personaje[] pers;
+        // colores para los personajes
+        ConsoleColor[] colors = {ConsoleColor.DarkYellow, 
+                                 ConsoleColor.Red,
+                                 ConsoleColor.Magenta, 
+                                 ConsoleColor.Cyan, 
+                                 ConsoleColor.DarkBlue };
+        const int lapCarcelFantasmas = 3000; // retardo para quitar el muro a los fantasmas
+        int lapFantasmas; // tiempo restante para quitar el muro
+        int numComida; // numero de casillas restantes con comida o vitamina
+        Random rnd; // generador de aleatorios
+                    // flag para mensajes de depuracion en consola
+        private bool DEBUG = true;
+
+        public Tablero(string file)
+        {
+            // Para la comprobacion de la existencia del archivo File.Exist creo...
+            if (File.Exists(file))
+            {
+                // Meter aquí TODO el código del método una vez terminado (para no liar).
+            }
+            else
+            {
+                throw new Exception("No existe el nivel seleccionado.");
+            }
+
+            // Abrimos flujo de archivo y se leen los niveles de levels/level0X.dat. ¡¡¡Ojo, meter luego ($"levels/{file}")!!!
+            StreamReader archivo = new StreamReader($"levels/level01.dat");
+
+            // Habrá dos lecturas:
+            // 1. Para determinar el tamaño de la matriz.
+            int fils = SacaFilas(archivo);
+            int cols = SacaColumnas(archivo);
+            Console.Write($"{fils} {cols}");
+            int[,] tableroNumeros = new int[fils, cols];
+
+            // 2. Para ir rellenando la matriz. // Mirar a ver si la parte 2 está bien, ya que no la he comprobado.
+            for (int i = 0; i < tableroNumeros.GetLength(0); i++)
+            {
+                // Lee la fila actual.
+                string s = archivo.ReadLine();
+
+                // En el array v va almacenando el contenido de la fila actual pero sin los espacios. Se guardan los números.
+                string[] v = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                // String -> Int.
+                int[] myInts = Array.ConvertAll(v, int.Parse);
+
+                for (int j = 0; j < tableroNumeros.GetLength(1); j++)
+                {
+                    // Rellena.
+                    tableroNumeros[i, j] = myInts[j];
+                    Console.Write($"{tableroNumeros[i, j]} ");
+                }
+            }
+
+            // Cerramos flujo.
+            archivo.Close();
+
+            /* FORMA QUE NO DEBERÍA DAR ERROR PERO ME PARECE POCO EFICIENTE
+
+            using (StreamReader archivo = new StreamReader($"levels/level01.dat"))
+            {
+                int fils = SacaFilas(archivo);
+                Console.Write($"{fils} ");
+            }
+
+            // Reopen the file and determine the number of columns.
+            using (StreamReader archivo = new StreamReader($"levels/level01.dat"))
+            {
+                int cols = SacaColumnas(archivo);
+                Console.Write($"{cols}");
+            }*/
+
+
+        }
+
+        #region Submétodos Tablero
+        private int SacaColumnas(StreamReader archivo)
+        {
+            int numCols = 0; // Inicialmente el tamaño es 0.
+            string leeCols = archivo.ReadLine().Replace(" ", ""); // Lee la linea y elimina los espacios en blanco.
+            numCols = leeCols.Length; // Saca la cantidad de columnas.
+            return numCols;
+        }
+
+        private int SacaFilas(StreamReader archivo)
+        {
+            int numFils = 1; // Empezamos a contar desde 1.
+
+            while(archivo.ReadLine() != null)
+            {
+                numFils++;
+            }
+
+            return numFils;
+        }
+        #endregion
+    }
+}
